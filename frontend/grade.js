db96949e7b7621f4fc797222c477ef826a622189
N-Latha <nlatha270@gmail.com>
2025-09-13 11:18:07 +0530

    Add files via upload

---
+++ frontend/grade.js
@@ -0,0 +1,16 @@
+function grade(marks){
+    return marks
+}
+let grading=grade(60)
+if(grading>=90)
+    console.log("A+")
+else if(grading>=60 || grading<90)
+    console.log("A")
+else if(grading>=45||grading<60)
+    console.log("B")
+else if(grading<45 || grading>=20)
+    console.log("P")
+else
+    console.log("F")
+
+b=30;
